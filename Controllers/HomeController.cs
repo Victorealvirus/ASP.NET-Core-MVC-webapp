668edb73e78160504b49ce40bd08e2bf111da6d3
adwait.nadkarni@centralsquare.com <adwait.nadkarni@centralsquare.com>
2020-05-28 13:50:42 -0700

    final list page commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BethanysPieShop.Models;
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,12 +13,19 @@ namespace BethanysPieShop.Controllers
                
         public HomeController(IPieRepository pieRepository)
         {
-
+            _pieRepository = pieRepository;
         }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Bethany's Pie Shop",
+                Pies = pies.ToList()
+            };
+            return View(homeViewModel);
         }
     }
 }
